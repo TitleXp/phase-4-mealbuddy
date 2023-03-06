f0a20601f4c1508c25ea96482658a95c560833ab
Surapat Mekvanich <titleyugio@gmail.com>
2023-03-06 12:09:02 -0800

    more validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,15 @@ class User < ApplicationRecord
     has_many :meals
     has_many :each_food_per_meal, through: :meals
     
-    validates :calorie_goal, :name, :age, :weight, :username, :password_digest, presence: true
-    validates :calorie_goal, :age, :weight numericality: {greater_than: 0}
+    validates :calorie_goal, :name, :dob, :weight, :username, :email :password_digest, presence: true
+    validates :calorie_goal, :weight numericality: {greater_than: 0}
+    # validates_date :dob, on_or_before: lambda { Date.current } #something about date here
+    validates :username, :email, uniqueness: true
+
+    validate :age_of_user
 
 
+    def age_of_user
+        
+    end
 end
--- db/migrate/20230306183752_create_users.rb
+++ db/migrate/20230306183752_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.integer :calorie_goal
       t.string :name
-      t.integer :age
+      t.string :email
+      t.date :dob
       t.integer :weight
       t.string :username
       t.string :password_digest
