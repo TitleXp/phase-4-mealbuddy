b724d2b101948072d82ef25deca02dcb6325f11b
Jaskiran Singh <jsingh8986@yahoo.com>
2023-03-07 11:03:46 -0800

    client side routing done

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <div>
-            
+            <Link to="/meals">
+                Meal Log    
+            </Link>
+
+            <Link to="/meals/food_item/new" >
+                Add To Meal
+            </Link>        
+
+            <Link to="/community">
+                Community
+            </Link> 
+
+            <Link to="/profile">
+                Profile
+            </Link>
         </div>
     );
 }
