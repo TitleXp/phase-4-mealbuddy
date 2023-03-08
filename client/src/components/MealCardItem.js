8ff4d36a4a9cc1de0b734e3a359537b1929c3315
Surapat Mekvanich <titleyugio@gmail.com>
2023-03-08 15:32:32 -0800

    technically full crud right now but stuff not rendering in state

--- client/src/components/MealCardItem.js
+++ client/src/components/MealCardItem.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
+import QuantityEditForm from './QuantityEditForm'
 
-// need setFoods here
  function MealCardItem({ food, user, setFoods }) {
 
+   
+   const [ showEditQtyForm, setShowEditQtyForm ] = useState(true)
+   
+   const handleClick = () => {
+     setShowEditQtyForm(currentValue => !currentValue)
+    }
+    
+    const [ editQuantity, setEditQuantity ] = useState({
+      quantity: food.quantity
+    })
+
+    const handleChange = (e) => {
+      setEditQuantity({...editQuantity, [e.target.name]: e.target.value})
+    }
 
   console.log ("this is food ", food)
 
@@ -12,11 +27,54 @@ import React from 'react'
     })
     setFoods(currentFood => currentFood.filter(element => element.id !== food.id))
 }
+const { id } = food
 
-const handleEditQuantity = () => {
+// const handleEditQuantity = (e) => {
+//     e.preventDefault()
+//     fetch(`/each_food_per_meals/${food.id}`,{
+//       method: "PATCH",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(editQuantity),
+//     })
+//     .then(response => {
+//       if(response.ok) {
+//         response.json()
+//         .then(editQty => {
+//           setFoods(currentFoods => {
+//             const updatedQuantity = currentFoods.map(qty =>{
+//               return qty.id === id ? editQty : qty
+//             })
+//             return updatedQuantity
+//           })
+//         })
+//       }
+//     })
+// }
+
+const handleEditQuantity = (e) => {
+  e.preventDefault();
+  console.log('handleEditQuantity function called')
+  fetch(`/each_food_per_meals/${food.id}`,{
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(editQuantity),
+  })
+  .then(editQty => {
+    setFoods(currentFoods => {
+      const updatedQuantity = currentFoods.map(food => {
+        return food.id === id ? editQty : food;
+      });
+      return updatedQuantity;
+    });
+  })
+} 
 
 
-}
+// console.log(id)
 
   return (
     <div>
@@ -26,7 +84,20 @@ const handleEditQuantity = () => {
       Fats: {food.food.fats}
       Proteins: {food.food.proteins}
       Quantity{food.quantity}
-      <button onClick={handleEditQuantity}>Edit quantity</button>
+
+      {showEditQtyForm ? 
+      <button onClick={handleClick}>Edit quantity</button> :
+      // 'edit quantity' form here
+      <> 
+        <button onClick={handleClick}>cancel change</button>
+        <form onSubmit={handleEditQuantity}>
+
+          <input type="number" name="quantity" value={editQuantity.quantity} onChange={handleChange}/>
+          <input type="submit" value="update" />
+        </form>
+      </>
+      }
+
       <br/>
       by: {user.username}
       <button onClick={handleDeleteFood}>Delete food within this meal</button>
---
+++ client/src/components/QuantityEditForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QuantityEditForm = () => {
+  return (
+    <div>QuantityEditForm</div>
+  )
+}
+
+export default QuantityEditForm
